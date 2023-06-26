3499468df008d3e393ec1c53c2f6f05302b1054c
Deyfran58 <137656348+Deyfran58@users.noreply.github.com>
2023-06-25 18:46:19 -0600

    Esto es una prueba de mi repo

---
+++ PRUEBA-DEYFRAN/src/prueba/deyfran/PRUEBADEYFRAN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba.deyfran;
+
+/**
+ *
+ * @author Admin
+ */
+public class PRUEBADEYFRAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.println("Esta es una prueba del repo");
+    }
+    
+}
