9e0f90da2a8d0ea7131918e5020601aa7dd784de
albdrag <137427702+albdrag@users.noreply.github.com>
2023-06-28 00:26:33 -0600

    Mi repo

--- PRUEBA-DEYFRAN/src/prueba/deyfran/PRUEBADEYFRAN.java
+++ PRUEBA-DEYFRAN/src/prueba/deyfran/PRUEBADEYFRAN.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prueba.deyfran;
 
-/**
- *
- * @author Admin
- */
 public class PRUEBADEYFRAN {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
            System.out.println("Esta es una prueba del repo");
            System.out.println("Esta es una prueba del repo JUSTIN");
+           System.out.println("Prueba de rama Jordan");
     }
     
 }
